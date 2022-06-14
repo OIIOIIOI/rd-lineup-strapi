00592569c15ccc56c439658bd85d92719d3ed22e
Sylvere Armange <sylvere.armange@gmail.com>
2022-06-14 23:44:51 +0200

    modified team controller

--- src/api/team/controllers/team.js
+++ src/api/team/controllers/team.js
@@ -6,4 +6,16 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::team.team');
+module.exports = createCoreController('api::team.team', ({ strapi }) => ({
+	async findOne(ctx) {
+		const { data, meta } = await super.findOne(ctx)
+		
+		// Add team skaters
+		data.attributes.skaters = await strapi.db.query('api::skater.skater').findMany({
+			where: { team: data.id },
+			populate: ['affinities', 'skater_role'],
+		});
+		
+		return { data, meta }
+	},
+}));
