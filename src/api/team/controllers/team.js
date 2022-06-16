845e92f284daaea949c6dc709f159965d400e6d8
Sylvere Armange <sylvere.armange@gmail.com>
2022-06-16 14:42:03 +0200

    removed obsolete controller override

--- src/api/team/controllers/team.js
+++ src/api/team/controllers/team.js
@@ -6,16 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::team.team', ({ strapi }) => ({
-	async findOne(ctx) {
-		const { data, meta } = await super.findOne(ctx)
-		
-		// Add team skaters
-		data.attributes.skaters = await strapi.db.query('api::skater.skater').findMany({
-			where: { team: data.id },
-			populate: ['affinities', 'skater_role'],
-		});
-		
-		return { data, meta }
-	},
-}));
+module.exports = createCoreController('api::team.team');
