3d7c8eb781c8c1fdd6e6ade9a2941e21f36c72cf
Sylvere Armange <sylvere.armange@gmail.com>
2022-06-17 00:46:25 +0200

    test cascade delete

--- src/api/game/controllers/game.js
+++ src/api/game/controllers/game.js
@@ -6,4 +6,23 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::game.game');
+module.exports = createCoreController('api::game.game', ({ strapi }) =>  ({
+	async delete(ctx)
+	{
+		const { id } = ctx.params
+		
+		await strapi.db.query('api::jam.jam').deleteMany({
+			where: {
+				game: {
+					id: {
+						$eq: id,
+					},
+				},
+			},
+		})
+		
+		return await super.delete(ctx)
+		// const response = await super.delete(ctx)
+		// return response
+	},
+}));
